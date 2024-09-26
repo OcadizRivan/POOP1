743980d9316467dac030c2ad08af16402e8571da
OcadizRivan <143865300+OcadizRivan@users.noreply.github.com>
2024-09-25 20:33:54 -0600

    Adicion de practica

---
+++ src/poop1/POOP1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1;
+
+/**
+ *
+ * @author nadia
+ */
+public class POOP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo con ANT");
+        System.out.println("Desde POO G3");
+        int a = 3;
+        Integer b=3;
+        String cadena = new String ("hola");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(cadena);
+    }
+    
+}
